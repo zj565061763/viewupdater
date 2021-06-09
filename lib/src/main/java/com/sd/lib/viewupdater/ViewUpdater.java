040b7989a8f1833e8b8c5336943c025767a5999b
zjun <565061763@qq.com>
2021-06-10 00:29:01 +0800

    format

--- lib/src/main/java/com/sd/lib/viewupdater/ViewUpdater.java
+++ lib/src/main/java/com/sd/lib/viewupdater/ViewUpdater.java
@@ -4,42 +4,36 @@ import android.view.View;
 
 public interface ViewUpdater {
     /**
-     * 设置更新回调对象
-     *
-     * @param updatable
+     * 设置更新对象
      */
     void setUpdatable(Updatable updatable);
 
     /**
-     * 通知设置的回调对象{@link #setUpdatable(Updatable)}进行更新
+     * 通知更新对象{@link #setUpdatable(Updatable)}
      */
     void notifyUpdatable();
 
     /**
      * 返回设置的view
-     *
-     * @return
      */
     View getView();
 
     /**
      * 设置view
-     *
-     * @param view
      */
     void setView(View view);
 
     /**
      * 是否已经开始监听
      *
-     * @return
+     * @return true-已经开始
      */
     boolean isStarted();
 
     /**
      * 开始监听
      *
-     * @return true-成功开始
+     * @return true-已经开始
      */
     boolean start();
 
